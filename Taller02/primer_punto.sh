67b3d3f7559e1e571a3ec6e0f1ede29a94c66f9e
Javier Sarmiento <javiersarmiento02@gmail.com>
2023-03-24 20:17:19 -0500

    updated scripts

--- Taller02/primer_punto.sh
+++ Taller02/primer_punto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir carpeta
-cd carpeta
-echo "1 2 3 4 5" > archivo1
-echo "6 7 8 9 10" > archivo2
+
+echo "1 2 3 4 5" > carpeta/archivo1
+echo "6 7 8 9 10" > carpeta/archivo2
+
 echo "Archivos creados exitosamente"
